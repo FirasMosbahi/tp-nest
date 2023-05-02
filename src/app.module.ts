99b8f88538cb261872be6edd52e3310d691aa2b0
firas <firasmosbahi10@gmail.com>
2023-05-02 08:58:36 +0100

    "jwt auth"

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,14 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, RequestMethod } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PremierModule } from './premier/premier.module';
 import { TodoModule } from './todo/todo.module';
 import { CommonModule } from './common/common.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { JwtModule } from '@nestjs/jwt';
 import * as process from 'process';
+import { DataSource } from 'typeorm';
+import { AuthMiddleware } from './middlewares/auth-middleware';
 
 @Module({
   imports: [
@@ -14,16 +17,52 @@ import * as process from 'process';
     CommonModule,
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: process.env['DB_HOST'],
-      port: Number.parseInt(process.env['DB_PORT']),
-      username: process.env['DB_USERNAME'],
-      password: process.env['DB_PASSWORD'],
-      database: process.env['DB_NAME'],
+      host: process.env['DB_HOST'] || 'localhost',
+      port: Number.parseInt(process.env['DB_PORT']) || 3306,
+      username: process.env['DB_USERNAME'] || 'root',
+      password: process.env['DB_PASSWORD'] || '',
+      database: process.env['DB_NAME'] || 'todo',
       entities: [],
       synchronize: true,
+    }),
+    JwtModule.register({
+      global: true,
+      secret: 'mySecretKey',
+      signOptions: { expiresIn: '3600s' },
     }),
   ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private dataSource: DataSource) {}
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(AuthMiddleware).forRoutes(
+      {
+        path: 'v1/todo',
+        method: RequestMethod.POST,
+      },
+      {
+        path: 'v1/todo/:id',
+        method: RequestMethod.PATCH,
+      },
+      {
+        path: 'v1/todo/:id',
+        method: RequestMethod.DELETE,
+      },
+      {
+        path: 'v2/todo',
+        method: RequestMethod.POST,
+      },
+      {
+        path: 'v2/todo/:id',
+        method: RequestMethod.PATCH,
+      },
+      {
+        path: 'v2/todo/:id',
+        method: RequestMethod.DELETE,
+      },
+    );
+    // .forRoutes({ path: 'todo', method: RequestMethod.POST });
+  }
+}
--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -2,10 +2,13 @@ import {
   Body,
   Controller,
   Delete,
-  Get, HttpException,
+  Get,
+  HttpException,
   Param,
   Patch,
-  Post, Put, Query,
+  Post,
+  Put,
+  Query,
   Version,
 } from '@nestjs/common';
 import { TodoService } from './todo.service';
@@ -13,7 +16,7 @@ import Todo from '../spec-classes/todo';
 import CreateTodoDTO from '../DTO/create-todo-DTO';
 import UpdateTodoDTO from '../DTO/update-todo-DTO';
 import TodoEntity from '../entities/todo-entity';
-import FindTodoFilterDTO from "../DTO/find-todo-filter-DTO";
+import FindTodoFilterDTO from '../DTO/find-todo-filter-DTO';
 
 @Controller('todo')
 export class TodoController {
@@ -55,27 +58,29 @@ export class TodoController {
   }
   @Delete('/:id')
   @Version('2')
-  async deleteTodoByIdFromDb(@Param('id') id : number):Promise<string>{
+  async deleteTodoByIdFromDb(@Param('id') id: number): Promise<string> {
     return await this.todoService.deleteTodoByIdFromBd(id);
   }
   @Delete('/soft/:id')
-  async softDeleteTodoByIdFromBd(@Param('id') id :number):Promise<string>{
+  async softDeleteTodoByIdFromBd(@Param('id') id: number): Promise<string> {
     return await this.todoService.softRemoveTodoByIdFromBd(id);
   }
   @Put('/recover/:id')
-  async recoverTodoByIdToBd(@Param('id') id : number) : Promise<TodoEntity>{
+  async recoverTodoByIdToBd(@Param('id') id: number): Promise<TodoEntity> {
     return await this.todoService.recoverTodoByIdToBd(id);
   }
   @Get('/count/perStatus')
-  async countTodosPerStatus(){
+  async countTodosPerStatus() {
     return await this.todoService.countTodosPerStatus();
   }
   @Get('/endpoint')
-  async todosEndpoint(@Query() filterDTO : FindTodoFilterDTO) : Promise<Array<TodoEntity>>{
+  async todosEndpoint(
+    @Query() filterDTO: FindTodoFilterDTO,
+  ): Promise<Array<TodoEntity>> {
     return await this.todoService.todosEndpoint(filterDTO);
   }
   @Get('/endpoint/:id')
-  async todoEndpointById(@Param('id') id : number) :Promise<TodoEntity>{
+  async todoEndpointById(@Param('id') id: number): Promise<TodoEntity> {
     return await this.todoService.todoEndpointById(id);
   }
 }
